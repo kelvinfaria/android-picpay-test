bb69aff3ee6f68025d52402150365ba62567ca3a
Kelvin <contato.kelvinfaria@gmail.com>
2021-01-06 20:33:42 -0300

    Log interceptor added to web service factory

--- app/src/main/java/com/picpay/desafio/android/di/networkModule.kt
+++ app/src/main/java/com/picpay/desafio/android/di/networkModule.kt
@@ -1,5 +1,6 @@
 package com.picpay.desafio.android.di
 
+import com.picpay.desafio.android.BuildConfig.*
 import com.picpay.desafio.android.data_remote.service.PicPayService
 import com.picpay.desafio.android.data_remote.utils.RequestWrapperInterface
 import com.picpay.desafio.android.network.BASE_URL
@@ -11,7 +12,11 @@ import org.koin.dsl.module
 
 val networkModule = module {
 
-    single { provideOkHttpClient() }
+    single {
+        provideOkHttpClient(
+            DEBUG && BUILD_TYPE == "debug"
+        )
+    }
 
     single {
         WebServiceFactory.createWebService(
@@ -20,6 +25,6 @@ val networkModule = module {
         ) as PicPayService
     }
 
-    single<RequestWrapperInterface> { RequestWrapper( androidApplication().resources) }
+    single<RequestWrapperInterface> { RequestWrapper(androidApplication().resources) }
 
 }
--- app/src/main/java/com/picpay/desafio/android/network/WebServiceFactory.kt
+++ app/src/main/java/com/picpay/desafio/android/network/WebServiceFactory.kt
@@ -3,6 +3,7 @@ package com.picpay.desafio.android.network
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
@@ -20,7 +21,18 @@ object WebServiceFactory {
         return retrofit.create()
     }
 
-    fun provideOkHttpClient(): OkHttpClient =
+    fun provideOkHttpClient(wasDebugVersion: Boolean): OkHttpClient =
         OkHttpClient.Builder()
+            .httpLoggingInterceptor(wasDebugVersion)
             .build()
+
+    private fun OkHttpClient.Builder.httpLoggingInterceptor(wasDebugVersion: Boolean) =
+        when (wasDebugVersion) {
+            true -> {
+                val interceptor = HttpLoggingInterceptor()
+                    .setLevel(HttpLoggingInterceptor.Level.BODY)
+                addInterceptor(interceptor)
+            }
+            else -> this
+        }
 }
